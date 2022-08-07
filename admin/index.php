db1f43bb35d05f2a0679aef5e45ec48d268af604
el-TIGON <xasanahmed811@gmail.com>
2022-08-07 18:16:03 +0300

    done

--- admin/index.php
+++ admin/index.php
@@ -21,8 +21,7 @@ include('includes/navbar.php');
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                       
                     </div>
 
                     <!-- Content Row -->
@@ -34,7 +33,9 @@ include('includes/navbar.php');
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Admins </div>
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Admins = 
+                                                
+                                            </div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                                 <?php
                                                 $query = "SELECT reg_id FROM registration ORDER BY reg_id ASC";
